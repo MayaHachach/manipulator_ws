62d9c119e0cd0a9eb39a708f71393223eabddb88
maya <mayahashash00@gmail.com>
2026-04-23 21:12:37 +0400

    initial commit - create manipulator_control pkg

---
+++ src/manipulator_control/launch/spawn_robot.launch.py
@@ -0,0 +1,109 @@
+import os
+import xacro
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, TimerAction
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    # Paths
+    urdf_path = os.path.join(
+        get_package_share_directory('open_manipulator_description'),
+        'urdf', 'open_manipulator_x', 'open_manipulator_x.urdf.xacro'
+    )
+
+    world_path = os.path.join(
+        get_package_share_directory('manipulator_control'),
+        'worlds', 'table_world.sdf'
+    )
+
+    # Read URDF as string
+    #with open(urdf_path, 'r') as f:
+    #    robot_description = f.read()
+    
+    controller_config = os.path.join(
+        get_package_share_directory('open_manipulator_bringup'),
+        'config', 'open_manipulator_x', 'hardware_controller_manager.yaml'
+    )
+
+    robot_description = xacro.process_file(urdf_path).toxml()
+
+    return LaunchDescription([
+
+        # 1. Start Gazebo with our world
+        ExecuteProcess(
+            cmd=['gz', 'sim', '-r', world_path],
+            output='screen'
+        ),
+
+        # 2. Robot State Publisher
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'robot_description': robot_description,
+                'use_sim_time': True
+            }]
+        ),
+
+        # 3. Spawn robot into Gazebo
+        Node(
+            package='ros_gz_sim',
+            executable='create',
+            arguments=[
+                '-name', 'open_manipulator_x',
+                '-string', robot_description,
+                '-x', '0.0',
+                '-y', '0.0',
+                '-z', '0.76',
+            ],
+            output='screen'
+        ),
+
+        # 4. Controller Manager — load controllers
+        # Wait 3 seconds for Gazebo to fully spawn the robot first
+        TimerAction(
+            period=3.0,
+            actions=[
+                Node(
+                    package='controller_manager',
+                    executable='spawner',
+                    arguments=['joint_state_broadcaster'],
+                    output='screen'
+                ),
+            ]
+        ),
+
+        TimerAction(
+            period=5.0,
+            actions=[
+                Node(
+                    package='controller_manager',
+                    executable='spawner',
+                    arguments=['arm_controller'],
+                    output='screen'
+                ),
+                Node(
+                    package='controller_manager',
+                    executable='spawner',
+                    arguments=['gripper_controller'],
+                    output='screen'
+                ),
+            ]
+        ),
+
+        # Bridge camera topic from Gazebo to ROS 2
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=[
+                '/gz/camera@sensor_msgs/msg/Image[gz.msgs.Image',
+                '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+            ],
+            output='screen'
+        ),
+
+    ])
